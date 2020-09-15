9c7636ca2fef2935058041262834bbd54f1aec8d
kmatt10 <kmatt10@gmail.com>
2020-09-15 11:50:57 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,5 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-	let mySrc = myImage.getAttribute('src');
-	if(mySrc === 'images/coffee-logo.png'){
-		myImage.setAttribute('src','images/burger-logo.jpg');
-	} else {
-		myImage.setAttribute('src','images/coffee-logo.png');
-	}
-}
-
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
